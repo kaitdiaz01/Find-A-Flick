6c3b1246a7a46f8aefd398d764434ecd00520ed4
nick quinn <nick.quinn@stadium1.com>
2022-04-12 10:30:27 -0400

    Adding fetch YT key again

--- Main/assets/js/script.js
+++ Main/assets/js/script.js
@@ -18,17 +18,51 @@ function getApi() {
 
 
 
-                // This calls the API, just update the url to have your key's name.
-          async function fetchYTKey() {
-            const url = 'https://yorkieportunus.herokuapp.com/store/find-a-flick utube'
-            const response = await fetch(url);
-            const key = await response.json();
-            return key;
-        }
-        // Call this wherever you need your key.
-        fetchYTKey().then((key) => {
-            secretKey = key.apiKey;
-            console.log(secretKey);
-        });
-      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
+                // This calls the API, just update the url to have your key's name.
+                async function fetchYTKey() {
+                  const url = 'https://yorkieportunus.herokuapp.com/store/find-a-flick utube'
+                  const response = await fetch(url);
+                  const key = await response.json();
+                  return key;
+              }
+              // Call this wherever you need your key.
+              fetchYTKey().then((key) => {
+                  secretKey = key.apiKey;
+                  console.log(secretKey);
+              });
+            
+          

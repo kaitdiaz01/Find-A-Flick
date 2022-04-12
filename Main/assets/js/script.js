41eb2875dae8bf095b24bbaff8a0ea3ede3f5937
Drago9082 <maxtiwari9082@gmail.com>
2022-04-12 14:10:49 -0400

    cleaned up the js

--- Main/assets/js/script.js
+++ Main/assets/js/script.js
@@ -1,48 +1,27 @@
-//test API
+// This calls the API, just update the url to have your key's name.
+async function fetchKey() {
+    const url = 'https://yorkieportunus.herokuapp.com/store/tmdb-p1'
+    const response = await fetch(url);
+    const key = await response.json();
+    return key;
+}
+// Call this wherever you need your key.
+fetchKey().then((key) => {
+    secretKey = key.apiKey;
+    console.log(secretKey);
+});
 
-function getApi() {
-    // fetch request gets a list of all the repos for the node.js organization
-    var requestUrl = 'https://api.themoviedb.org/3/movie/550?api_key=821ac2213e8e35805cb8bcd84865bc4c';
-  
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data)
-      });
-    }
-
-    getApi();
-          // This calls the API, just update the url to have your key's name.
-          async function fetchKey() {
-            const url = 'https://yorkieportunus.herokuapp.com/store/tmdb-p1'
-            const response = await fetch(url);
-            const key = await response.json();
-            return key;
-        }
-        // Call this wherever you need your key.
-        fetchKey().then((key) => {
-            secretKey = key.apiKey;
-            console.log(secretKey);
-        });
-      
-        
-              // This calls the API, just update the url to have your key's name.
-         
-          // This calls the API, just update the url to have your key's name.
-        
-          // This calls the API, just update the url to have your key's name.
-          async function fetchKeyYouTube() {
-            const url = 'https://yorkieportunus.herokuapp.com/store/find-a-flick-utube'
-            const response = await fetch(url);
-            const key = await response.json();
-            return key;
-        }
-        // Call this wherever you need your key.
-        fetchKeyYouTube().then((key) => {
-            secretKey = key.apiKey;
-            console.log(secretKey);
-        });
+// This calls the API, just update the url to have your key's name.
+async function fetchKeyYouTube() {
+    const url = 'https://yorkieportunus.herokuapp.com/store/find-a-flick-utube'
+    const response = await fetch(url);
+    const key = await response.json();
+    return key;
+}
+// Call this wherever you need your key.
+fetchKeyYouTube().then((key) => {
+    secretKey = key.apiKey;
+    console.log(secretKey);
+});
       
     
